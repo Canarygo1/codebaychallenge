85bee48eb53197f577d0f38ff8934d23b3664627
Alejandro <alejandro.cruz@atani.com>
2021-01-09 22:42:51 +0000

    feature: basic web structure finished

--- src/App.tsx
+++ src/App.tsx
@@ -21,11 +21,11 @@ function App() {
           <h2>A Big Title</h2>
         </a>
         <div className="d-none d-sm-block">
-          <span>Home</span>
-          <span>Event</span>
-          <span>Accesories</span>
-          <span>Apparel</span>
-          <span>Fetured Items</span>
+          <span className="mr-2">Home</span>
+          <span className="mr-2">Event</span>
+          <span className="mr-2">Accesories</span>
+          <span className="mr-2">Apparel</span>
+          <span className="mr-2">Fetured Items</span>
         </div>
         <select className="d-block d-sm-none">
           <option value="Home">Home</option>
@@ -77,52 +77,85 @@ function App() {
         standard dummy text ever since the 1500s, when an unknown printer took a galley of
         type and scrambled it to make a type specimen book.</span>
       <div className="container-fluid">
-      <div className="row mt-4 ">
-        <div className="col-6  col-md-3 d-flex justify-content-center">
-          <div className={"row"}>
-            <img src="card_example.png" className="img-thumbnail" alt=""/>
+        <div className="row mt-4 ">
+          <div className="col-6  col-md-3 d-flex justify-content-center">
+            <div className={"row"}>
+              <img src="card_example.png" className="img-thumbnail" alt=""/>
 
-            <h6 className="col-12">Intro Copy Headline</h6>
-            <span className="col-12">Item Category</span>
-            <span className="col-12">$88.00</span>
+              <h6 className="col-12">Intro Copy Headline</h6>
+              <span className="col-12">Item Category</span>
+              <span className="col-12">$88.00</span>
+            </div>
+
+          </div>
+          <div className="col-6  col-md-3 d-flex justify-content-center">
+            <div className={"row"}>
+              <img src="card_example.png" className="img-thumbnail" alt=""/>
+
+              <h6 className="col-12">Intro Copy Headline</h6>
+              <span className="col-12">Item Category</span>
+              <span className="col-12">$88.00</span>
+            </div>
+
+          </div>
+          <div className="col-6  col-md-3 d-flex justify-content-center">
+            <div className={"row"}>
+              <img src="card_example.png" className="img-thumbnail" alt=""/>
+
+              <h6 className="col-12">Intro Copy Headline</h6>
+              <span className="col-12">Item Category</span>
+              <span className="col-12">$88.00</span>
+            </div>
+
+          </div>
+          <div className="col-6  col-md-3 d-flex justify-content-center">
+            <div className={"row"}>
+              <img src="card_example.png" className="img-thumbnail" alt=""/>
+
+              <h6 className="col-12">Intro Copy Headline</h6>
+              <span className="col-12">Item Category</span>
+              <span className="col-12">$88.00</span>
+            </div>
+
           </div>
 
-        </div>
-        <div className="col-6  col-md-3 d-flex justify-content-center">
-          <div className={"row"}>
-            <img src="card_example.png" className="img-thumbnail" alt=""/>
 
-            <h6 className="col-12">Intro Copy Headline</h6>
-            <span className="col-12">Item Category</span>
-            <span className="col-12">$88.00</span>
+        </div>
+        <div className="container-fluid bg-light pb-5">
+          <div className="row mt-4 ">
+            <div className="col-12 col-md-3 ">
+              <div className={"row"}>
+                <h5 className="col-12">Quick Links</h5>
+                <span className="col-12">Home</span>
+                <span className="col-12">Event</span>
+                <span className="col-12">Support</span>
+                <span className="col-12">Contact</span>
+              </div>
+            </div>
+            <div className="col-12 col-md-6">
+              <div className={"row"}>
+                <h5 className="col-12">Announcements / Promoted Content </h5>
+                <span className="col-12">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+        standard dummy text ever since the 1500s, when an unknown printer took a galley of
+        type and scrambled it to make a type specimen book.</span>
+              </div>
+            </div>
+            <div className="col-12 col-md-3 ">
+              <div className={"row"}>
+                <h5 className="col-12" >Coming Soon</h5>
+                <img src="carousel.png" className=" img-fluid"/>
+              </div>
+            </div>
           </div>
-
         </div>
-        <div className="col-6  col-md-3 d-flex justify-content-center">
-          <div className={"row"}>
-            <img src="card_example.png" className="img-thumbnail" alt=""/>
-
-            <h6 className="col-12">Intro Copy Headline</h6>
-            <span className="col-12">Item Category</span>
-            <span className="col-12">$88.00</span>
-          </div>
-
-        </div>
-        <div className="col-6  col-md-3 d-flex justify-content-center">
-          <div className={"row"}>
-            <img src="card_example.png" className="img-thumbnail" alt=""/>
-
-            <h6 className="col-12">Intro Copy Headline</h6>
-            <span className="col-12">Item Category</span>
-            <span className="col-12">$88.00</span>
-          </div>
-
-        </div>
-
+      </div>
+      <div className="row mt-4 justify-content-center ">
+        <span className="mr-2">@2012 AcmeWidgets.</span>
+        <span className="mr-2" >All Rights Reserved.</span>
+        <span className="mr-2">Support: Support@AcmeWidgets.com</span>
 
       </div>
       </div>
-    </div>
 
   );
 }
